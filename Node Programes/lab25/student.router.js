89b98440202ff3ef95184b3c578049f1d98322d7
Krunal Vaishnani <140934304+KrunalVaishnani@users.noreply.github.com>
2024-08-17 19:57:01 +0530

    Delete Node Programes directory

--- Node Programes/lab25/student.router.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const studentRouter = express.Router();
-const { addStudent } = require('./student.controller');
-
-studentRouter.post('/',addStudent);
-
-module.exports=studentRouter;
